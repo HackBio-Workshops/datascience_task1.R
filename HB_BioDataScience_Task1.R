e8dea57cd51929b3e36b212b26bb2910cf3e45d6
Paschal Ugwu <106428248+paschalugwu@users.noreply.github.com>
2022-10-18 20:27:32 +0100

    Update HB_BioDataScience_Task1.R

--- HB_BioDataScience_Task1.R
+++ HB_BioDataScience_Task1.R
@@ -39,3 +39,79 @@ item <- unique(astrocyte_data$T..Gene.names[1:10])
 itemCount <- as.vector(table(astrocyte_data$T..Gene.names[1:10]))
 pie (x = itemCount, labels = item, radius = 0.5, col = 11:20, main = 'Pie chart for astrocyte species')
 pie (x = itemCount, labels = item, radius = 1.0, edge = T, border = 'red', col = 11:20, main = 'Pie chart for astrocyte species')
+
+# Let's arrange these plots as we usually see in publications
+par(mfrow = c(2,2))
+
+        
+# LET US NOW GO FULLY INTO GGPLOTS
+# We are going to work with a new dataset
+# About data set: Growth and Toxigenicity of A. flavus on Resistant and Susceptible Peanut Genotypes. This study seeks to determine the reaction of peanut genotypes to  Aflatoxigenic and non-aflatoxigenic A. flavus inoculation and also determine the mechanisms of their resistance. It was established that non-aflatoxigenic A. flavus grows faster than aflatoxigenic A. flavus. There was no significant difference in the incidence and severity of the A. flavus resistant genotypes (L027B and ICGV-03401) however, there were significant differences between resistant genotypes and the susceptible check Manipinta.  This study also confirmed that non-aflatoxigenic A. flavus inoculation did not lead to aflatoxin production. Non-aflatoxigenic A. flavus identified could serve as  a good biocontrol against aflatoxin contamination under field conditions. Additionally, peanut genotypes with resistance to post-harvest aflatoxin accumulation will resist the growth of A. flavus and subsequent aflatoxin accumulation.
+
+# Install ggplot2
+install.packages("ggplot2")
+library(ggplot2)
+
+# Import .csv file in R
+aflatoxin_data <- read.csv(file.choose())
+aflatoxin_data
+
+# Start with defining the base data for ggplot
+pl <- ggplot(data = aflatoxin_data)
+
+# To the base, add what you want to plot and color
+pl + geom_bar(aes(x=GENOTYPES))
+
+# Plot frequency of data from a single column
+pl + geom_bar(aes(y=Isolate))
+
+# Flip the coordinates
+pl + geom_bar(aes(y=Isolate))+coord_flip()
+
+# See the frequency of each variable within the species.
+# Install.package(reshape2)
+# Melt the data to have something understandable by ggplot better
+install.packages("reshape2")
+install.packages("Rcpp")
+library(reshape2)
+melted_aflatoxin_data <- melt(aflatoxin_data)
+# Stacked/continuous bar plot.
+ggplot(data = aflatoxin_data) + geom_bar(aes(x=GENOTYPES, y=Isolate, fill = GENOTYPES), stat = "identity")
+# Multiple bar plot
+ggplot(data = aflatoxin_data) + geom_bar(aes(x=GENOTYPES, y=Isolate, fill = GENOTYPES), stat = "identity", position = 'dodge')
+
+#Pie chart
+ggplot(data = aflatoxin_data) + geom_bar(aes(x=factor(1), fill = GENOTYPES), width = 1) + coord_polar(theta = 'y')
+
+# Remember to add theme you like
+ggplot(data = aflatoxin_data) + geom_bar(aes(x=factor(1), fill = GENOTYPES), width = 1) + coord_polar(theta = 'y') + theme_classic()
+
+# Histogram
+# pl <- ggplot(data = aflatoxin_data)
+pl + geom_histogram(aes(x=S_Day2, fill = GENOTYPES)) + facet_grid(. ~GENOTYPES)
+#Add a color parameter
+pl + geom_histogram(aes(x=S_Day2, fill = GENOTYPES), color = "black") + facet_grid(. ~GENOTYPES)
+
+# Scatter plots
+pl + geom_point(aes(x = S_Day2, y = S_Day8, color = 'red'))
+# Color by Genotype; quite automated
+pl + geom_point(aes(x = S_Day2, y = S_Day8, color = GENOTYPES)) + theme_bw() + ggtitle(label = 'Scatterplot', subtitle = 'Days')
+
+# Boxplot plus a theme
+pl + geom_boxplot(notch = F, aes(x= GENOTYPES, y = S_Day8, fill = GENOTYPES)) + theme_classic()
+
+# Heat map
+# Using your melting methods, create heat map with ggtiles
+meltCorData <- melt(cor(aflatoxin_data[3:8]))
+# Set your new ggplot
+hm <- ggplot(data = meltCorData)
+hm + geom_tile(aes(x = Var1, y = Var2, fill = value))
+# Coloring in heatmaps
+hm + geom_tile(aes(x = Var1, y = Var2, fill = value)) + scale_fill_gradient2(low = 'red', high = 'blue')
+# You can also use hex code
+hm + geom_tile(aes(x = Var1, y = Var2, fill = value)) + scale_fill_gradient2(low = '#1687ee', high = '#d27C1c')
+# Change x and y labels
+hm + geom_tile(aes(x = Var1, y = Var2, fill = value)) + scale_fill_gradient2() + xlab('First Variable') + ylab('Second Variable')
+
+# Let's arrange these plots as we usually see in publications
+par(mfrow = c(2,2))
